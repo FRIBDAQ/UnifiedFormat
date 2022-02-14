a66601888bc67177aa46c4061b4773eca19273ea
Ron Fox <fox@nscl.msu.edu>
2022-02-14 12:58:48 -0500

    Added test templates for v10::CRingPhysicsEventCountITem.

---
+++ v10/v10counttests.cpp
@@ -0,0 +1,47 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  v10counttests.cpp
+ *  @brief: 
+ */
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/Asserter.h>
+#include "Asserts.h"
+
+class v10counttest : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(v10counttest);
+    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST_SUITE_END();
+    
+private:
+
+public:
+    void setUp() {
+        
+    }
+    void tearDown() {
+        
+    }
+protected:
+    void test_1();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(v10counttest);
+
+void v10counttest::test_1()
+{
+}
