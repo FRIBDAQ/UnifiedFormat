0088732eb9d56aeb5850780a57c32b15d6a4782e
Ron Fox <fox@nscl.msu.edu>
2022-03-02 14:17:19 -0500

    -  Tested construction -  Tested run number getter/setter.

--- v10/v10statetests.cpp
+++ v10/v10statetests.cpp
@@ -24,9 +24,16 @@
 #include "CRingStateChangeItem.cpp"
 #include "DataFormat.h"
 
+#include <time.h>
+#include <string.h>
+
 class v10statetest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(v10statetest);
-    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST(construct_1);
+    CPPUNIT_TEST(construct_2);
+    
+    CPPUNIT_TEST(run_1);
+    CPPUNIT_TEST(run_2);
     CPPUNIT_TEST_SUITE_END();
     
 private:
@@ -39,11 +46,60 @@ public:
         
     }
 protected:
-    void test_1();
+    void construct_1();
+    void construct_2();
+    
+    void run_1();
+    void run_2();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(v10statetest);
 
-void v10statetest::test_1()
+// Construction should give a proper state change item.
+void v10statetest::construct_1()
 {
+    time_t now = time(nullptr);
+    v10::CRingStateChangeItem item;
+    v10::pStateChangeItem p = reinterpret_cast<v10::pStateChangeItem>(item.getItemPointer());
+    
+    EQ(v10::BEGIN_RUN, p->s_header.s_type);
+    EQ(sizeof(v10::StateChangeItem), size_t(p->s_header.s_size));
+    EQ(uint32_t(0), p->s_runNumber);
+    EQ(uint32_t(0), p->s_timeOffset);
+    ASSERT(p->s_Timestamp - now <= 1);
+    EQ(0, strcmp(p->s_title, ""));
+}
+// fully parameterized construction.
+
+void v10statetest::construct_2()
+{
+    time_t now = time(nullptr);
+    v10::CRingStateChangeItem item(v10::PAUSE_RUN, 123, 100, now, "This is a title");
+    v10::pStateChangeItem p = reinterpret_cast<v10::pStateChangeItem>(item.getItemPointer());
+    
+    EQ(v10::PAUSE_RUN, p->s_header.s_type);
+    EQ(sizeof(v10::StateChangeItem), size_t(p->s_header.s_size));
+    
+    EQ(uint32_t(123), p->s_runNumber);
+    EQ(uint32_t(100), p->s_timeOffset);
+    EQ(now, time_t(p->s_Timestamp));
+    EQ(0, strcmp(p->s_title, "This is a title"));
+}
+//  Can get run number:
+
+void v10statetest::run_1()
+{
+    time_t now = time(nullptr);
+    v10::CRingStateChangeItem item(v10::PAUSE_RUN, 123, 100, now, "This is a title");
+
+    
+    EQ(uint32_t(123), item.getRunNumber());
+}
+void v10statetest::run_2()
+{
+    time_t now = time(nullptr);
+    v10::CRingStateChangeItem item(v10::PAUSE_RUN, 123, 100, now, "This is a title");
+
+    item.setRunNumber(200);
+    EQ(uint32_t(200), item.getRunNumber());
 }
