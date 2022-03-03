a1a87368b7536f698f25bf159ac1416def5965d6
Ron Fox <fox@nscl.msu.edu>
2022-03-03 07:47:10 -0500

    Continu testing state change itemss for v10.

--- v10/v10statetests.cpp
+++ v10/v10statetests.cpp
@@ -34,6 +34,18 @@ class v10statetest : public CppUnit::TestFixture {
     
     CPPUNIT_TEST(run_1);
     CPPUNIT_TEST(run_2);
+    
+    CPPUNIT_TEST(elapsed_1);
+    CPPUNIT_TEST(elapsed_2);
+    
+    CPPUNIT_TEST(divisor);
+    CPPUNIT_TEST(computeelapsed);
+    
+    CPPUNIT_TEST(title_1);
+    CPPUNIT_TEST(title_2);
+    
+    CPPUNIT_TEST(tstamp_1);
+    CPPUNIT_TEST(tstamp_2);
     CPPUNIT_TEST_SUITE_END();
     
 private:
@@ -51,6 +63,18 @@ protected:
     
     void run_1();
     void run_2();
+    
+    void elapsed_1();
+    void elapsed_2();
+    
+    void divisor();
+    void computeelapsed();
+    
+    void title_1();
+    void title_2();
+    
+    void tstamp_1();
+    void tstamp_2();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(v10statetest);
@@ -95,6 +119,8 @@ void v10statetest::run_1()
     
     EQ(uint32_t(123), item.getRunNumber());
 }
+// See if we can set run number:
+
 void v10statetest::run_2()
 {
     time_t now = time(nullptr);
@@ -102,4 +128,77 @@ void v10statetest::run_2()
 
     item.setRunNumber(200);
     EQ(uint32_t(200), item.getRunNumber());
+}
+// See if we can fetch elapsed time
+
+void v10statetest::elapsed_1()
+{
+    time_t now = time(nullptr);
+    v10::CRingStateChangeItem item(v10::PAUSE_RUN, 123, 100, now, "This is a title");
+    
+    EQ(uint32_t(100), item.getElapsedTime());
+}
+// See if we can set the elapsed time:
+
+void v10statetest::elapsed_2()
+{
+    time_t now = time(nullptr);
+    v10::CRingStateChangeItem item(v10::PAUSE_RUN, 123, 100, now, "This is a title");
+    
+    item.setElapsedTime(200);
+    EQ(uint32_t(200), item.getElapsedTime());
+}
+// divisor is hard coded to 1:
+
+void v10statetest::divisor()
+{
+    time_t now = time(nullptr);
+    v10::CRingStateChangeItem item(v10::PAUSE_RUN, 123, 100, now, "This is a title");
+    
+    EQ(uint32_t(1), item.getTimeDivisor());
+}
+// Elapsed time is the same as the raw time:
+
+void v10statetest::computeelapsed()
+{
+    time_t now = time(nullptr);
+    v10::CRingStateChangeItem item(v10::PAUSE_RUN, 123, 100, now, "This is a title");
+    
+    EQ(float(100), item.computeElapsedTime());
+}
+// Can fetch title:
+void v10statetest::title_1()
+{
+    time_t now = time(nullptr);
+    v10::CRingStateChangeItem item(v10::PAUSE_RUN, 123, 100, now, "This is a title");
+    
+    EQ(std::string("This is a title"), item.getTitle());
+    
+}
+// Can set title.
+void v10statetest::title_2()
+{
+    time_t now = time(nullptr);
+    v10::CRingStateChangeItem item(v10::PAUSE_RUN, 123, 100, now, "This is a title");
+    
+    item.setTitle("Changed the title");
+    EQ(std::string("Changed the title"), item.getTitle());
+}
+
+// Can get time_t
+void v10statetest::tstamp_1()
+{
+    time_t now = time(nullptr);
+    v10::CRingStateChangeItem item(v10::PAUSE_RUN, 123, 100, now, "This is a title");
+    
+    EQ(now, item.getTimestamp());
+}
+// Can set time_t
+void v10statetest::tstamp_2()
+{
+    time_t now = time(nullptr);
+    v10::CRingStateChangeItem item(v10::PAUSE_RUN, 123, 100, now, "This is a title");
+    
+    item.setTimestamp(now+100);
+    EQ(now+100, item.getTimestamp());
 }
