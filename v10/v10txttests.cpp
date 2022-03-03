007595498214ccc69c258b1c01ee701e5de2c9c0
Ron Fox <fox@nscl.msu.edu>
2022-03-03 10:02:48 -0500

    Texted simple constructor.

--- v10/v10txttests.cpp
+++ v10/v10txttests.cpp
@@ -24,10 +24,13 @@
 #include "CRingTextItem.h"
 #include "DataFormat.h"
 
+#include <time.h>
+#include <string>
+#include <vector>
 
 class v10txttest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(v10txttest);
-    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST(construct_1);
     CPPUNIT_TEST_SUITE_END();
     
 private:
@@ -40,11 +43,23 @@ public:
         
     }
 protected:
-    void test_1();
+    void construct_1();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(v10txttest);
 
-void v10txttest::test_1()
+// Construct with type and strings.
+void v10txttest::construct_1()
 {
+    std::vector<std::string> strings;
+    time_t now = time(nullptr);
+    v10::CRingTextItem item(v10::MONITORED_VARIABLES, strings);
+    const v10::TextItem* p = reinterpret_cast<const v10::TextItem*>(item.getItemPointer());
+    
+    EQ(MONITORED_VARIABLES, p->s_header.s_type);
+    EQ(uint32_t(sizeof(v10::TextItem)), p->s_header.s_size);
+    
+    EQ(uint32_t(0), p->s_timeOffset);
+    ASSERT(p->s_timeOffset - now <= 1);
+    EQ(uint32_t(0), p->s_stringCount);
 }
