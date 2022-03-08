c582756461dc4f8de0d7ef9a5ff8bea199ef2ee4
Ron Fox <fox@nscl.msu.edu>
2022-03-08 12:51:50 -0500

    Add template for testing v10::RingItemFactory.

---
+++ v10/v10factorytests.cpp
@@ -0,0 +1,49 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  v10factorytests.cpp
+ *  @brief: Test the V10 ring item factory.
+ */
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/Asserter.h>
+#include "Asserts.h"
+#include "RingItemFactory.h"
+
+
+class v10factorytest : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(v10factorytest);
+    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST_SUITE_END();
+    
+private:
+
+public:
+    void setUp() {
+        
+    }
+    void tearDown() {
+        
+    }
+protected:
+    void test_1();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(v10factorytest);
+
+void v10factorytest::test_1()
+{
+}
