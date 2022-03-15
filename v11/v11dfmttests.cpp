a9c8f698b856155672ccc275f9bcd959b519c9a5
Ron Fox <fox@nscl.msu.edu>
2022-03-15 11:32:30 +0000

    Added template test class for v11::CDataFormatItem.

---
+++ v11/v11dfmttests.cpp
@@ -0,0 +1,50 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  v11dfmttests.cpp
+ *  @brief: V11 Data format Item tests.
+ */
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/Asserter.h>
+#include "Asserts.h"
+#include "CDataFormatItem.h"
+#include "DataFormat.h"
+
+
+class v11dfmttest : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(v11dfmttest);
+    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST_SUITE_END();
+    
+private:
+
+public:
+    void setUp() {
+        
+    }
+    void tearDown() {
+        
+    }
+protected:
+    void test_1();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(v11dfmttest);
+
+void v11dfmttest::test_1()
+{
+}
