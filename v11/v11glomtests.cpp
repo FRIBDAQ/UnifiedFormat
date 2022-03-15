cf24f90b120c4bbdf905b7d255351ba5e72400f5
Ron Fox <fox@nscl.msu.edu>
2022-03-15 13:58:18 +0000

    Empty test suite for v11::CGlomParameters class

---
+++ v11/v11glomtests.cpp
@@ -0,0 +1,50 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  v11glomtests.cpp
+ *  @brief: Tests for v11::CGlomParameters
+ */
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/Asserter.h>
+#include "Asserts.h"
+#include "CGlomParameters.h"
+#include "DataFormat.h"
+
+
+class v11glomtest : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(v11glomtest);
+    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST_SUITE_END();
+    
+private:
+
+public:
+    void setUp() {
+        
+    }
+    void tearDown() {
+        
+    }
+protected:
+    void test_1();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(v11glomtest);
+
+void v11glomtest::test_1()
+{
+}
