2c29c24ee09ba57177a20fb2e1a35d1da30b6309
Ron Fox <fox@nscl.msu.edu>
2022-03-15 16:06:51 +0000

    Completed testing v11::CGlomParameters

--- v11/v11glomtests.cpp
+++ v11/v11glomtests.cpp
@@ -27,7 +27,13 @@
 
 class v11glomtest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(v11glomtest);
-    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST(construct);
+    CPPUNIT_TEST(getticks);
+    CPPUNIT_TEST(isbuilding);
+    CPPUNIT_TEST(policy);
+    
+    CPPUNIT_TEST(bodyhdr_1);
+    CPPUNIT_TEST(bodyhdr_2);
     CPPUNIT_TEST_SUITE_END();
     
 private:
@@ -40,11 +46,57 @@ public:
         
     }
 protected:
-    void test_1();
+    
+    void construct();
+    void getticks();
+    void isbuilding();
+    void policy();
+    void bodyhdr_1();
+    void bodyhdr_2();
+    
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(v11glomtest);
 
-void v11glomtest::test_1()
+void v11glomtest::construct()
 {
+    v11::CGlomParameters item(100, true, CGlomParameters::last);
+    EQ(v11::EVB_GLOM_INFO, item.type());
+    EQ(sizeof(v11::GlomParameters), size_t(item.size()));
+    const v11::GlomParameters* pItem =
+        reinterpret_cast<const v11::GlomParameters*>(item.getItemPointer());
+    EQ(uint32_t(0), pItem->s_mbz);
+    EQ(uint64_t(100), pItem->s_coincidenceTicks);
+    ASSERT(pItem->s_isBuilding != 0);
+    EQ(v11::GLOM_TIMESTAMP_LAST, pItem->s_timestampPolicy);
+    
+}
+void v11glomtest::getticks()
+{
+    v11::CGlomParameters item(100, true, CGlomParameters::last);
+    EQ(uint64_t(100), item.coincidenceTicks());
+}
+void v11glomtest::isbuilding()
+{
+    v11::CGlomParameters item(100, true, CGlomParameters::last);
+    ASSERT(item.isBuilding());
+}
+void v11glomtest::policy()
+{
+    v11::CGlomParameters item(100, true, CGlomParameters::last);
+    EQ(CGlomParameters::last, item.timestampPolicy());
+}
+
+void v11glomtest::bodyhdr_1()
+{
+    v11::CGlomParameters item(100, true, CGlomParameters::last);
+    ASSERT(item.getBodyHeader() == nullptr);
+}
+void v11glomtest::bodyhdr_2()
+{
+    v11::CGlomParameters item(100, true, CGlomParameters::last);
+    CPPUNIT_ASSERT_THROW(
+        item.setBodyHeader(200, 1, 1),
+        std::logic_error
+    );
 }
