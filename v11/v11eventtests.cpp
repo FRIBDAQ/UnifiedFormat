431e1437e8e0626f7d39d3ade20d0dfab452a0d1
Ron Fox <fox@nscl.msu.edu>
2022-03-16 18:08:51 +0000

    Completed testing of v11::CPHysicsEventItem.

--- v11/v11eventtests.cpp
+++ v11/v11eventtests.cpp
@@ -24,6 +24,8 @@
 #include "CPhysicsEventItem.h"
 #include "DataFormat.h"
 
+#include <stdexcept>
+
 class v11eventtest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(v11eventtest);
     CPPUNIT_TEST(construct_1);
@@ -38,6 +40,23 @@ class v11eventtest : public CppUnit::TestFixture {
     CPPUNIT_TEST(bodypointer_2);
     CPPUNIT_TEST(bodypointer_3);
     CPPUNIT_TEST(bodypointer_4);
+    
+    CPPUNIT_TEST(hasbodyhdr_1);
+    CPPUNIT_TEST(hasbodyhdr_2);
+    
+    CPPUNIT_TEST(tstamp_1);
+    CPPUNIT_TEST(tstamp_2);
+    
+    CPPUNIT_TEST(sid_1);
+    CPPUNIT_TEST(sid_2);
+    
+    CPPUNIT_TEST(btype_1);
+    CPPUNIT_TEST(btype_2);
+    
+    CPPUNIT_TEST(bheader_1);
+    CPPUNIT_TEST(bheader_2);
+    CPPUNIT_TEST(bheader_3);
+    CPPUNIT_TEST(bheader_4);
     CPPUNIT_TEST_SUITE_END();
     
 private:
@@ -62,6 +81,23 @@ protected:
     void bodypointer_2();
     void bodypointer_3();
     void bodypointer_4();
+    
+    void hasbodyhdr_1();
+    void hasbodyhdr_2();
+    
+    void tstamp_1();
+    void tstamp_2();
+    
+    void sid_1();
+    void sid_2();
+    
+    void btype_1();
+    void btype_2();
+    
+    void bheader_1();
+    void bheader_2();
+    void bheader_3();
+    void bheader_4();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(v11eventtest);
@@ -246,5 +282,149 @@ void v11eventtest::bodypointer_4()
     pb = reinterpret_cast<const uint16_t*>(item.getBodyPointer());
     for (uint16_t i = 0; i < 100; i++) {
         EQ(pb[i], i);
+    }
+}
+// Construction w/o body header gives body header:
+
+void v11eventtest::hasbodyhdr_1()
+{
+    v11::CPhysicsEventItem item(1000);
+    ASSERT(!item.hasBodyHeader());
+}
+// construction with body header has body header.
+
+void v11eventtest::hasbodyhdr_2()
+{
+    v11::CPhysicsEventItem item(123456789, 1, 0, 100);
+    ASSERT(item.hasBodyHeader());
+}
+// get timetamp for non body header is a logic error.
+
+void v11eventtest::tstamp_1()
+{
+    v11::CPhysicsEventItem item(100);
+    CPPUNIT_ASSERT_THROW(
+        item.getEventTimestamp(),
+        std::logic_error
+    );
+}
+// get timestamp for body header is ok.
+void v11eventtest::tstamp_2()
+{
+    v11::CPhysicsEventItem item(123456789, 1, 0, 100);
+    uint64_t stamp;
+    CPPUNIT_ASSERT_NO_THROW(stamp = item.getEventTimestamp());
+    EQ(uint64_t(123456789), stamp);
+}
+// Non bodyheader item. getSourceId throws.
+
+void v11eventtest::sid_1()
+{
+    v11::CPhysicsEventItem item(100);
+    CPPUNIT_ASSERT_THROW(
+        item.getSourceId(),
+        std::logic_error
+    );
+}
+// body header getSourceId is normal and correct:
+
+void v11eventtest::sid_2()
+{
+    v11::CPhysicsEventItem item(123456789, 1, 0, 100);
+    uint32_t stamp;
+    CPPUNIT_ASSERT_NO_THROW(stamp = item.getSourceId());
+    EQ(uint32_t(1), stamp);
+}
+
+// barrier type:
+
+void v11eventtest::btype_1()
+{
+    v11::CPhysicsEventItem item(100);
+    CPPUNIT_ASSERT_THROW(
+        item.getBarrierType(),
+        std::logic_error
+    );
+}
+void v11eventtest::btype_2()
+{
+    v11::CPhysicsEventItem item(123456789, 1, 0, 100);
+    uint32_t stamp;
+    CPPUNIT_ASSERT_NO_THROW(stamp = item.getBarrierType());
+    EQ(uint32_t(0), stamp);
+}
+// Get body header from headerless item.
+
+void v11eventtest::bheader_1()
+{
+    v11::CPhysicsEventItem item( 100);
+    ASSERT(item.getBodyHeader() == nullptr);
+}
+// Body header from header item gives correct pointer.
+void v11eventtest::bheader_2()
+{
+    v11::CPhysicsEventItem item(123456789, 1, 0, 100);
+    v11::pBodyHeader pB = reinterpret_cast<v11::pBodyHeader>(item.getBodyHeader());
+    v11::pPhysicsEventItem pItem = reinterpret_cast<v11::pPhysicsEventItem>(item.getItemPointer());
+    v11::pBodyHeader pExpected = &(pItem->s_body.u_hasBodyHeader.s_bodyHeader);
+    EQ(pExpected, pB);
+}
+// insert body header into filled bodyheader-less item:
+
+void v11eventtest::bheader_3()
+{
+    v11::CPhysicsEventItem item(100);
+    
+    
+    uint16_t* p = reinterpret_cast<uint16_t*>(item.getBodyCursor());
+    
+    for (int i =0; i < 100; i++) {
+        *p++ = i;
+    }
+    item.setBodyCursor(p);
+    item.updateSize();
+    size_t priorSize = item.size();
+    
+    item.setBodyHeader(100, 1, 0);
+    EQ(priorSize + sizeof(v11::BodyHeader) - sizeof(uint32_t), size_t(item.size()));
+    
+    v11::pBodyHeader pB = reinterpret_cast<v11::pBodyHeader>(item.getBodyHeader());
+    EQ(uint32_t(sizeof(v11::BodyHeader)), pB->s_size);
+    EQ(uint64_t(100), pB->s_timestamp);
+    EQ(uint32_t(1), pB->s_sourceId);
+    EQ(uint32_t(0), pB->s_barrier);
+    
+    p = reinterpret_cast<uint16_t*>(item.getBodyPointer());
+    for (uint16_t i =0; i < 100; i++) {
+        EQ(p[i], i);
+    }
+}
+// writes in place if there's already one:
+void v11eventtest::bheader_4()
+{
+    v11::CPhysicsEventItem item(123456789, 2, 0, 100);
+    
+    
+    uint16_t* p = reinterpret_cast<uint16_t*>(item.getBodyCursor());
+    
+    for (int i =0; i < 100; i++) {
+        *p++ = i;
+    }
+    item.setBodyCursor(p);
+    item.updateSize();
+    size_t prior = item.size();
+    
+    item.setBodyHeader(100, 1, 0);
+    EQ(prior, size_t(item.size()));   // Size stays the same.
+    
+    v11::pBodyHeader pB = reinterpret_cast<v11::pBodyHeader>(item.getBodyHeader());
+    EQ(uint32_t(sizeof(v11::BodyHeader)), pB->s_size);
+    EQ(uint64_t(100), pB->s_timestamp);
+    EQ(uint32_t(1), pB->s_sourceId);
+    EQ(uint32_t(0), pB->s_barrier);
+    
+    p = reinterpret_cast<uint16_t*>(item.getBodyPointer());
+    for (uint16_t i =0; i < 100; i++) {
+        EQ(p[i], i);
     }
 }
