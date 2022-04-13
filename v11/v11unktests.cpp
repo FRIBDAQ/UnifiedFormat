625a2265c30cb8ed462f829d5ad91f254719a615
Ron Fox <fox@nscl.msu.edu>
2022-04-13 16:57:17 +0000

    Tested body header stuff for v11::CUnknonwFragment

--- v11/v11unktests.cpp
+++ v11/v11unktests.cpp
@@ -32,6 +32,10 @@ class v11unktest : public CppUnit::TestFixture {
     
     CPPUNIT_TEST(bodyptr_1);
     CPPUNIT_TEST(bodyptr_1);
+    
+    CPPUNIT_TEST(bodyhdr_1);
+    CPPUNIT_TEST(bodyhdr_2);
+    CPPUNIT_TEST(bodyhdr_3);
     CPPUNIT_TEST_SUITE_END();
     
 private:
@@ -49,6 +53,10 @@ protected:
     
     void bodyptr_1();
     void bodyptr_2();
+    
+    void bodyhdr_1();
+    void bodyhdr_2();
+    void bodyhdr_3();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(v11unktest);
@@ -108,7 +116,7 @@ void v11unktest::bodyptr_1()
 
 void v11unktest::bodyptr_2()
 {
-     std::vector<std::uint16_t> data;
+    std::vector<std::uint16_t> data;
     for (int i =0; i < 100; i++) {
         data.push_back(i);
     }
@@ -119,4 +127,53 @@ void v11unktest::bodyptr_2()
         reinterpret_cast<const v11::EventBuilderFragment*>(item.getItemPointer());
     const uint8_t* pBody = reinterpret_cast<const uint8_t*>(item.getBodyPointer());
     EQ(&(pItem->s_body[0]), pBody);
+}
+//  always has a body header:
+
+void v11unktest::bodyhdr_1()
+{
+    std::vector<std::uint16_t> data;
+    for (int i =0; i < 100; i++) {
+        data.push_back(i);
+    }
+    v11::CUnknownFragment item(
+        0x1234567890, 1, 0, data.size()*sizeof(uint16_t),  data.data()
+    );
+    ASSERT(item.hasBodyHeader());
+}
+// can get the correct body header:
+
+void v11unktest::bodyhdr_2()
+{
+    std::vector<std::uint16_t> data;
+    for (int i =0; i < 100; i++) {
+        data.push_back(i);
+    }
+    v11::CUnknownFragment item(
+        0x1234567890, 1, 0, data.size()*sizeof(uint16_t),  data.data()
+    );
+    const v11::BodyHeader* pBh = reinterpret_cast<const v11::BodyHeader*>(item.getBodyHeader());
+    const v11::EventBuilderFragment* pItem =
+        reinterpret_cast<const v11::EventBuilderFragment*>(item.getItemPointer());
+    EQ(&(pItem->s_bodyHeader), pBh);
+    
+}
+// can modify body header:
+
+void v11unktest::bodyhdr_3()
+{
+    std::vector<std::uint16_t> data;
+    for (int i =0; i < 100; i++) {
+        data.push_back(i);
+    }
+    v11::CUnknownFragment item(
+        0x1234567890, 1, 0, data.size()*sizeof(uint16_t),  data.data()
+    );
+    item.setBodyHeader(0x111111111, 2, 5);
+    const v11::BodyHeader* pBh =
+        reinterpret_cast<const v11::BodyHeader*>(item.getBodyHeader());
+    EQ(sizeof(v11::BodyHeader), size_t(pBh->s_size));
+    EQ(uint64_t(0x111111111), pBh->s_timestamp);
+    EQ(uint32_t(2), pBh->s_sourceId);
+    EQ(uint32_t(5), pBh->s_barrier);
 }
