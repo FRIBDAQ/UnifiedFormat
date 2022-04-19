d1d3c261bf6f71a84b2e312857b3b445ea6802f9
Ron Fox <fox@nscl.msu.edu>
2022-04-19 15:42:43 +0000

    Added module in which to provide unit tests for the v11 factory.

---
+++ v11/v11factoryTests.cpp
@@ -0,0 +1,47 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     FRIB
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  v11factoryTests.cpp
+ *  @brief: Test for the V11 ring item factory.
+ */
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/Asserter.h>
+#include "Asserts.h"
+
+class v11facttest : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(v11facttest);
+    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST_SUITE_END();
+    
+private:
+
+public:
+    void setUp() {
+        
+    }
+    void tearDown() {
+        
+    }
+protected:
+    void test_1();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(v11facttest);
+
+void v11facttest::test_1()
+{
+}
