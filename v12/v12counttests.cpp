5f125179d5f53cea99c810105b6107a2ab1f2a76
Ron Fox <fox@nscl.msu.edu>
2022-05-05 15:08:05 +0000

    Add empty test module for v12::CRingPhysicsEventCOuntItem

---
+++ v12/v12counttests.cpp
@@ -0,0 +1,47 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     FRIB
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  v12counttests.cpp
+ *  @brief:  Test v12::CRingPhysicsEventCountItem
+ */
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/Asserter.h>
+#include "Asserts.h"
+
+class v12counttest : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(v12counttest);
+    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST_SUITE_END();
+    
+private:
+
+public:
+    void setUp() {
+        
+    }
+    void tearDown() {
+        
+    }
+protected:
+    void test_1();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(v12counttest);
+
+void v12counttest::test_1()
+{
+}
