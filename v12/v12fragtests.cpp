56e3134f11f2b3c2887b4ae032cde6b2a0bfb952
Ron Fox <fox@nscl.msu.edu>
2022-05-23 12:04:21 +0000

    Tested native accessor methods of v12::CRingFragmentItem

--- v12/v12fragtests.cpp
+++ v12/v12fragtests.cpp
@@ -30,6 +30,12 @@ class v12fragtest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(v12fragtest);
     CPPUNIT_TEST(construct_1);
     CPPUNIT_TEST(construct_2);
+    
+    CPPUNIT_TEST(getts);        // These are from the per type
+    CPPUNIT_TEST(getsid);       // methods.
+    CPPUNIT_TEST(payloadsize);
+    CPPUNIT_TEST(payloadptr);
+    CPPUNIT_TEST(btype);
     CPPUNIT_TEST_SUITE_END();
     
 private:
@@ -44,6 +50,11 @@ public:
 protected:
     void construct_1();
     void construct_2();
+    void getts();
+    void getsid();
+    void payloadsize();
+    void payloadptr();
+    void btype();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(v12fragtest);
@@ -82,3 +93,62 @@ void v12fragtest::construct_2()
     
     EQ(0, memcmp(payload, pItem->s_body, sizeof(payload)));
 }
+// Get the timestamp fromt he event.
+void v12fragtest::getts()
+{
+    uint8_t payload[100];
+    for (int i =0; i < sizeof(payload); i++) {
+        payload[i] = i;
+    }
+    v12::CRingFragmentItem item(0x1234568790, 2, sizeof(payload), payload, 1);
+    
+    EQ(uint64_t(0x1234568790), item.timestamp());
+}
+// get the source id:
+void v12fragtest::getsid()
+{
+    uint8_t payload[100];
+    for (int i =0; i < sizeof(payload); i++) {
+        payload[i] = i;
+    }
+    v12::CRingFragmentItem item(0x1234568790, 2, sizeof(payload), payload, 1);
+    EQ(uint32_t(2), item.source());
+}
+// get the payload size:
+
+void v12fragtest::payloadsize()
+{
+    uint8_t payload[100];
+    for (int i =0; i < sizeof(payload); i++) {
+        payload[i] = i;
+    }
+    v12::CRingFragmentItem item(0x1234568790, 2, sizeof(payload), payload, 1);
+    EQ(sizeof(payload), item.payloadSize());
+    
+}
+// paylod pointer is correct:
+
+void v12fragtest::payloadptr()
+{
+    uint8_t payload[100];
+    for (int i =0; i < sizeof(payload); i++) {
+        payload[i] = i;
+    }
+    v12::CRingFragmentItem item(0x1234568790, 2, sizeof(payload), payload, 1);
+    v12::pEventBuilderFragment pItem =
+        reinterpret_cast<v12::pEventBuilderFragment>(item.getItemPointer());
+    void* pSb = pItem->s_body;
+    EQ(pSb, item.payloadPointer());    // we already verified contents.
+}
+// get barrier type:
+
+void v12fragtest::btype()
+{
+    uint8_t payload[100];
+    for (int i =0; i < sizeof(payload); i++) {
+        payload[i] = i;
+    }
+    v12::CRingFragmentItem item(0x1234568790, 2, sizeof(payload), payload, 1);
+    EQ(uint32_t(1), item.barrierType());
+}
+
