b57afa8d97adf6a79da5d26a45b00378659d30b1
Ron Fox <fox@nscl.msu.edu>
2022-06-28 14:54:12 +0000

    Add an abstract data source which we'll build in order to provide undifferentiated ring items.

---
+++ examples/evtdump/DataSource.cpp
@@ -0,0 +1,41 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     FRIB
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  DataSource.cpp
+ *  @brief: Implementation of the non pure vir. methods of DataSource.
+ */
+
+#include "DataSource.h"
+#include <RingItemFactoryBase.h>
+
+/**
+ * constructor
+ *  Just saves the factory pointer - note that we gain ownershp of the factory and,
+ *  therefore, it's deleted on our destruction.
+ */
+DataSource::DataSource(RingItemFactoryBase* pFactory) :
+    m_pFactory(pFactory)
+{}
+
+/**
+ * destructor
+ *    destroys the factory.
+ */
+DataSource::~DataSource()
+{
+    delete m_pFactory;
+}
---
+++ examples/evtdump/DataSource.h
@@ -0,0 +1,47 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     FRIB
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+#ifndef DATASOURCE_H
+#define DATASOURCE_H
+
+/** @file  DataSource.h
+ *  @brief Works with factories to provide a data source for undifferntiaed ring items.
+ * @note Abstract base class for FdDataSource, StreamDataSource and RingDataSource
+ */
+
+class CRingItem;
+class RingItemFactoryBase;
+
+/**
+ * @class DataSource
+ *    Pure abstract data source which uses a factory's ring item getters to
+ *    provide ring item from a data source.  Since the factory provides this,
+ *    we'll need concrete classes:
+ *    - FdDataSource - give data from a file descriptor.
+ *    - StreamDataSource -give data from a stream.
+ *    - RingDataSource -give data from a ringbuffer.
+ */
+class DataSource {
+protected:
+    RingItemFactoryBase* m_pFactory;
+public:
+    DataSource(RingItemFactoryBase* pFactory);
+    virtual ~DataSource();
+    virtual CRingItem* getItem() = 0;
+}
+
+
+#endif
