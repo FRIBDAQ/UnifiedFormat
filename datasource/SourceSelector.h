2a668749b65595c09783e1c71c87d250a7d78bf7
Ron Fox <fox@frib.msu.edu>
2026-01-29 09:59:23 -0500

    Issue #15 make the data source stuff public
    Pell out the make data source function and put it in the ufmt
    namespace.

---
+++ datasource/SourceSelector.h
@@ -0,0 +1,12 @@
+#ifndef SOURCESELECTOR_H
+#define SOURCESELECTOR_H
+#include <string>
+class DataSource;
+class RingItemFactoryBase;
+
+namespac ufmt {
+    DataSource*
+    makeDataSource(RingItemFactoryBase* pFactory, const std::string& strUrl);
+}
+
+#endif
